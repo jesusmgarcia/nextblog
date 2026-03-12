3c76eb4c51f8c5ceedba38995ebaea12001f9654
Jesus M. Garcia <jesusmgarciadev@gmail.com>
2026-03-12 16:29:11 +0100

    add navbar and dark mode

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,8 +1,10 @@
+import Navbar from '@/components/home/header/navbar';
 import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <div className='flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black'>
+    <div>
+      <Navbar />
       <Button>Hello</Button>
     </div>
   );
